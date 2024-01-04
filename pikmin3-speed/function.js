8277de7d9a9771ed612bf4b0741b11e74f4db60c
nimzo6689 <nimzo6689@users.noreply.github.com>
2024-01-04 10:10:19 +0900

    Update function.js

--- pikmin3-speed/function.js
+++ pikmin3-speed/function.js
@@ -1,4 +1,3 @@
 window.function = function (itemName, weight, maximumCarryNo) {
-  console.log(`itemName=${itemName}, ${JSON.stringify(weight)} + ${JSON.stringify(maximumCarryNo)}`);
-  return weight + maximumCarryNo;
+  return weight.value + maximumCarryNo.value;
 }
